030e9fc40a2b3fff75b12ded3dc93bc308558dcd
Pritesh Kumar <pritesh@gmail.com>
2022-02-24 16:52:12 +0530

    query params intro and assignment with post

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function (req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
-
-router.get("/random", function (req, res) {
-    res.send("hi there")
-})
-
-
-router.get("/test-api", function (req, res) {
-    res.send("hi FunctionUp")
-})
-
-
-router.get("/test-api-2", function (req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3", function (req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4", function (req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5", function (req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6", function (req, res) {
-    res.send({ a: 56, b: 45 })
-})
-
-router.post("/test-post", function (req, res) {
-    res.send([23, 45, 6])
-})
-
-
-router.post("/test-post-2", function (req, res) {
-    res.send({ msg: "hi", status: true })
-})
-
-router.post("/test-post-3", function (req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log(req.body)
-
-    res.send({ msg: "hi", status: true })
-})
-
-router.post("/test-post-4", function (req, res) {
-    let arr = [12, "functionup"]
-    let ele = req.body.element
-    arr.push(ele)
-    res.send({ msg: arr, status: true })
-})
-
+// QUERY PARAMS
 // localhost:3000/get-query-1?myCoolVar=24&xyz=hiFunctionUP
 router.get("/get-query-1", function (req, res) {
     let data = req.query
