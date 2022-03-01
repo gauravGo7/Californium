f9a56533be19246eb33b23675a2774895a659141
Pritesh Kumar <pritesh@gmail.com>
2022-03-01 16:32:01 +0530

    db session 3- mixed | CRUD | moment

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -10,22 +10,18 @@ const bookSchema = new mongoose.Schema( {
         indianPrice: String,
         europePrice: String,
     },
-    sales: {type: Number, default: 10}
+    sales: {type: Number, default: 10},
+    
+    // " best boook on earth"   [ "Nodejs in detail" , "mongodb in detail", "fronend in detail"] 
+    // {
+        // "ch1 ": "awesome intro to JS",
+        // "ch2" : "intro to nodejs",
+        // "ch3" : "intro to db"
+    //  }
+    summary :  mongoose.Schema.Types.Mixed,
+    isDeleted: Boolean //true on book deletion i.e you flag the document/data as isDeleted: true..(mark "dirty")
+
 }, { timestamps: true });
 
 
 module.exports = mongoose.model('Book', bookSchema) //users
-
-//Validation:
-//require:true
-//unique
-// default
-
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
--- src/routes/route.js
+++ src/routes/route.js
@@ -16,4 +16,26 @@ router.post("/createBook", BookController.createBook  )
 
 router.get("/getBooksData", BookController.getBooksData)
 
+router.post("/updateBooks", BookController.updateBooks)
+router.post("/deleteBooks", BookController.deleteBooks)
+
+//MOMENT JS
+const moment = require('moment');
+router.get("/dateManipulations", function (req, res) {
+    
+    // const today = moment();
+    // let x= today.add(10, "days")
+
+    // let validOrNot= moment("29-02-1991", "DD-MM-YYYY").isValid()
+    // console.log(validOrNot)
+    
+    const dateA = moment('01-01-1900', 'DD-MM-YYYY');
+    const dateB = moment('01-01-2000', 'DD-MM-YYYY');
+
+    let x= dateB.diff(dateA, "days")
+    console.log(x)
+
+    res.send({ msg: "all good"})
+})
+
 module.exports = router;
