3e395977e556ea7be9396ecb7f2c3f5121a1db48
Sabiha <sabiha.khan@functionup.org>
2022-08-05 15:52:26 +0530

    session examples

---
+++ src/introduction/intro.js
@@ -0,0 +1,10 @@
+const batchName = "Plutonium"
+
+let printName = function() {
+    console.log('Bathc name is ', batchName)
+}
+
+module.exports.name = batchName
+module.exports.printName = printName
+
+
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,20 @@
 const express = require('express');
-
+const abc = require('../introduction/intro')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+    console.log('My batch is', abc.name)
+    abc.printName()
+    res.send('My second ever api!')
 });
 
+
+router.get('/test-you', function(req, res){
+    res.send('This is the second routes implementation')
+})
+
+router.get('/give-me-students-data',function(req, res){
+
+})
 module.exports = router;
 // adding this comment for no reason
