46b775e069e4751f6f37e93770edbd1a8b5f07c8
Sabiha <sabiha.khan@functionup.org>
2022-08-08 16:07:38 +0530

    refactor example

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,16 +5,34 @@ const router = express.Router();
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
     abc.printName()
+    logger.welcome()
+
     res.send('My second ever api!')
 });
 
-
-router.get('/test-you', function(req, res){
-    res.send('This is the second routes implementation')
+router.get('/students', function (req, res){
+    let students = ['Sabiha', 'Neha', 'Akash']
+    res.send(students)
 })
 
-router.get('/give-me-students-data',function(req, res){
+router.get('/student-details/:name', function(req, res){
+    /*
+    params is an attribute inside request that contains 
+    dynamic values.
+    This value comes from the request url in the form of an 
+    object where key is the variable defined in code 
+    and value is what is sent in the request
+    */
 
+    let requestParams = req.params
+
+    // JSON strigify function helps to print an entire object
+    // We can use any ways to print an object in Javascript, JSON stringify is one of them
+    console.log("This is the request "+ JSON.stringify(requestParams))
+    let studentName = requestParams.name
+    console.log('Name of the student is ', studentName)
+    
+    res.send('Dummy response')
 })
+
 module.exports = router;
-// adding this comment for no reason
