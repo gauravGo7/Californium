515245f7d9f309c02494b0e339ef13991e3e28fb
sabiha-thorium <miamoscovitzs@gmail.com>
2022-12-07 15:56:20 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # Plutonium
-Backend cohort Feb 2022 - May 2022
+Backend cohort Nov 2022 - Mar 2023
 
 
-# Assignment
-https://docs.google.com/document/d/1HanTHEnUW0d2M7KOCTWrnqzwRWOkLUU0X_x_TwWnyTs/edit?usp=sharing
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,38 +1,8 @@
 const express = require('express');
-const abc = require('../introduction/intro')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    logger.welcome()
-
-    res.send('My second ever api!')
+    res.send('My first ever api!')
 });
-
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
-})
-
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
-
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    
-    res.send('Dummy response')
-})
 
 module.exports = router;
