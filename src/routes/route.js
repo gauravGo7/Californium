3de2be2721aaa8e5a4535947691c703838a60d41
gauravGo7 <grvgoswami2007@gmail.com>
2022-12-14 17:15:05 +0530

    assignment completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -158,6 +158,43 @@ router.post( "/post-query-2", function (req, res){
     }
     res.send( {data: finalArr , status: true})
 })
-
+let persons= [
+    {
+    name: "PK",
+    age: 10,
+    votingStatus: false
+ },
+ {
+    name: "SK",
+    age: 20,
+    votingStatus: false
+ },
+ {
+    name: "AA",
+    age: 70,
+    votingStatus: false
+ },
+ {
+    name: "SC",
+    age: 5,
+    votingStatus: false
+ },
+ {
+    name: "HO",
+    age: 40,
+    votingStatus: false
+ }
+ ]
+ 
+router.post("/myQuery",function(req,res){
+    let vote=req.query.votingage
+    let finalpersons=[]
+    for(let i=0;i<persons.length;i++){
+        if (persons[i].age>=vote){
+            (finalpersons.push(persons[i]))
+            persons[i].votingStatus=true
+        }}
+    res.send( {data: finalpersons , status: true})
+})
 
 module.exports = router;
